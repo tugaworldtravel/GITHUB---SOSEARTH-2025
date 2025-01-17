46c222f55dc84cfec6c2aba3c966cb786b388a6b
tugaworldtravel <tugaworldtravel@gmail.com>
2025-01-17 12:47:31 +0000

    Atualizando o config.js para refletir as mudanças nas páginas

--- config.js
+++ config.js
@@ -1,31 +1,31 @@
 var config = {
     // style: 'mapbox://styles/mapbox/streets-v12',
-    // leave commented to use Mapbox Standard Style
     accessToken: 'pk.eyJ1IjoidHVnYXdvcmxkdHJhdmVsIiwiYSI6ImNtNXZkMzEwMzAwbGUyanF6dXVtOHQ5anoifQ.9VMIO6xw7aUD_uSileHqSw',
     showMarkers: true,
     markerColor: '#3FB1CE',
-    //projection: 'equirectangular',
-    //Read more about available projections here
-    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
     inset: true,
     insetOptions: {
         markerColor: 'orange'
     },
     insetPosition: 'bottom-right',
     theme: 'dark',
-    use3dTerrain: false, //set true for enabling 3D maps.
+    use3dTerrain: false, // Set true for enabling 3D maps.
     auto: false,
     title: 'SOS EARTH',
     subtitle: 'The Storytelling Template helps you create an awesome animated map story with ease.',
     byline: 'By Daniel Rodrigues',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    
+    // Atualizando o caminho do index
+    index: 'earth.html',  // Agora, "earth.html" será a página principal no config
+
     chapters: [
         {
             id: 'slug-style-id',
             hidden: true,
             title: 'World',
             location: {
-                center: [-10,20],
+                center: [-10, 20],
                 zoom: 2.3,
                 pitch: -10,
                 bearing: 0
@@ -33,19 +33,8 @@ var config = {
             mapAnimation: 'flyTo',
             rotateAnimation: true,
             callback: '',
-            onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1,
-                //     duration: 5000
-                // }
-            ],
-            onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
-            ]
+            onChapterEnter: [],
+            onChapterExit: []
         },
         {
             id: 'Portugal-chapter',
@@ -63,19 +52,8 @@ var config = {
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1,
-                //     duration: 5000
-                // }
-            ],
-            onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
-            ]
+            onChapterEnter: [],
+            onChapterExit: []
         },
         {
             id: 'DC-chapter',
@@ -84,18 +62,12 @@ var config = {
             title: 'Washington, D.C.',
             style: 'mapbox://styles/tugaworldtravel/cm5wfmwp600rx01s34i715irn',
             image: './assets/washington-dc.jpg',
-            description: 'The second chapter flies to Washington, D.C., updates the camera pitch, and slowly rotates. <br>  <br> Washington, D.C., the capital of the United States, is a vibrant city known for its iconic landmarks, including the White House, the U.S. Capitol, and the Washington Monument. It serves as the political heart of the nation and a center for history, culture, and international diplomacy.',
+            description: 'The second chapter flies to Washington, D.C., updates the camera pitch, and slowly rotates. Washington, D.C., the capital of the United States, is a vibrant city known for its iconic landmarks.',
             location: {
                 center: [-77.020636, 38.886900],
                 zoom: 8.5,
                 pitch: 60,
-                bearing: -43.2,
-                // flyTo additional controls-
-                // These options control the flight curve, making it move
-                // slowly and zoom out almost completely before starting
-                // to pan.
-                //speed: 2, // make the flying slow
-                //curve: 1, // change the speed at which it zooms out
+                bearing: -43.2
             },
             mapAnimation: 'flyTo',
             rotateAnimation: true,
@@ -110,7 +82,7 @@ var config = {
             title: 'Geneva',
             image: './assets/geneva.jpg',
             style: 'mapbox://styles/tugaworldtravel/cm5idrk8q001w01rzd6ts6zy1',
-            description: 'Geneva, Switzerland, is a picturesque city nestled along the shores of Lake Geneva, surrounded by the Alps and Jura mountains. Known as a global hub for diplomacy and finance, it is home to numerous international organizations, including the United Nations and the Red Cross.',
+            description: 'Geneva, Switzerland, is a picturesque city nestled along the shores of Lake Geneva, surrounded by the Alps and Jura mountains.',
             location: {
                 center: [6.15116, 46.20595],
                 zoom: 12.52,
@@ -129,7 +101,7 @@ var config = {
             hidden: false,
             title: 'Buenos Aires',
             image: './assets/buenos-aires.jpg',
-            description: 'Buenos Aires, the capital of Argentina, is a dynamic city known for its European-inspired architecture, vibrant tango culture, and rich culinary scene. Often called the "Paris of South America," it blends historic charm with modern energy.  You can add as many chapters as you need, just copy the JSON data and make changes.',
+            description: 'Buenos Aires, the capital of Argentina, is a dynamic city known for its European-inspired architecture, vibrant tango culture, and rich culinary scene.',
             location: {
                 center: [-58.54195, -34.71600],
                 zoom: 4,
@@ -148,7 +120,7 @@ var config = {
             hidden: false,
             title: 'Tokyo',
             image: './assets/tokyo.jpg',
-            description: 'Tokyo, Japan\'s bustling capital, is known for its skyscrapers, shopping, and pop culture. It offers a mix of traditional and modern attractions, including historic temples and cutting-edge technology.',
+            description: 'Tokyo, Japan\'s bustling capital, is known for its skyscrapers, shopping, and pop culture.',
             location: {
                 center: [139.6917, 35.6895],
                 zoom: 10,
