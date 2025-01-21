aa78b8a19a17d9b852805c160ec51b7014248ada
tugaworldtravel <tugaworldtravel@gmail.com>
2025-01-21 15:36:47 +0000

    “TESTE.html” 

--- config-map.js
+++ config-map.js
@@ -1,4 +1,6 @@
+
 var config = {
+    // style: 'mapbox://styles/mapbox/streets-v12',
     accessToken: 'pk.eyJ1IjoidHVnYXdvcmxkdHJhdmVsIiwiYSI6ImNtNXZkMzEwMzAwbGUyanF6dXVtOHQ5anoifQ.9VMIO6xw7aUD_uSileHqSw',
     showMarkers: true,
     markerColor: '#3FB1CE',
@@ -8,15 +10,15 @@ var config = {
     },
     insetPosition: 'bottom-right',
     theme: 'dark',
-    use3dTerrain: false, // Ativar 3D se necessário
+    use3dTerrain: false, // Set true for enabling 3D maps.
     auto: false,
     title: 'SOS EARTH',
-    subtitle: 'A plataforma para mapear eventos climáticos',
-    byline: 'Por Daniel Rodrigues',
-    footer: 'Fonte: documentações, etc. <br> Criado usando <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    subtitle: 'The Storytelling Template helps you create an awesome animated map story with ease.',
+    byline: 'By Daniel Rodrigues',
+    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     
-    // Definindo o índice inicial para 'map.html'
-    index: 'map.html',
+    // Atualizando o caminho do index
+    index: 'earth.html',  // Agora, "earth.html" será a página principal no config
 
     chapters: [
         {
@@ -36,70 +38,32 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'image-chapter',
+            id: 'Portugal-chapter',
+            alignment: 'left',
             hidden: false,
-            title: '',
-            image: './assets/AVEIRO.jpeg', // A primeira imagem que ocupa a tela inteira
+            title: 'Portugal',
+            image: './assets/AVEIRO.jpeg',
+            description: 'The first chapter contains a title, image, and camera view for San Francisco, California. Update the chapter data to make it your own.',
             location: {
-                center: [20, 40],
-                zoom: 2.3,
-                pitch: -10,
+                center: [-8.3675, 40.7333], // Updated coordinates for Sever do Vouga, Portugal
+                zoom: 8.5,
+                pitch: 20,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [
-                // Alterando o fundo para a imagem de "AVEIRO.jpeg"
-                () => {
-                    document.body.style.backgroundImage = "url('./assets/AVEIRO.jpeg')";
-                    document.body.style.backgroundSize = 'cover';
-                    document.body.style.backgroundPosition = 'center';
-                    document.body.style.backgroundAttachment = 'fixed';
-                    document.getElementById('map').style.display = 'none'; // Oculta o mapa
-                }
-            ],
-            onChapterExit: [
-                () => {
-                    document.body.style.backgroundImage = 'none'; // Remove a imagem de fundo
-                    document.getElementById('map').style.display = 'block'; // Mostra o mapa novamente
-                }
-            ]
-        },
-        {
-            id: 'text-chapter',
-            hidden: false,
-            title: 'Combater as alterações climáticas',
-            image: '',
-            description: 'Texto explicativo sobre a importância de combater as alterações climáticas.',
-            location: {
-                center: [-8.3675, 40.7333],
-                zoom: 4,
-                pitch: 15,
-                bearing: 0
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [
-                // Garantindo que o texto vai ser exibido na tela
-                () => {
-                    document.getElementById('map').style.display = 'none'; // Esconde o mapa quando o capítulo de texto aparece
-                }
-            ],
-            onChapterExit: [
-                () => {
-                    document.getElementById('map').style.display = 'block'; // Reexibe o mapa quando o capítulo sai
-                }
-            ]
+            onChapterEnter: [],
+            onChapterExit: []
         },
         {
             id: 'DC-chapter',
+            alignment: 'right',
             hidden: false,
             title: 'Washington, D.C.',
-            style: 'mapbox://styles/tugaworldtravel/cm5wfmwp600rx01s34i715irn', // Estilo personalizado
+            style: 'mapbox://styles/tugaworldtravel/cm5wfmwp600rx01s34i715irn',
             image: './assets/washington-dc.jpg',
-            description: 'Washington, D.C. é a capital dos EUA, e está sempre no centro das decisões políticas globais.',
+            description: 'The second chapter flies to Washington, D.C., updates the camera pitch, and slowly rotates. Washington, D.C., the capital of the United States, is a vibrant city known for its iconic landmarks.',
             location: {
                 center: [-77.020636, 38.886900],
                 zoom: 8.5,
@@ -113,34 +77,62 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'image-chapter-2',
+            id: 'Geneva-chapter',
+            alignment: 'left',
             hidden: false,
-            title: '',
-            image: './assets/SOS_14.JPG', // Segunda imagem que ocupa toda a tela
+            title: 'Geneva',
+            image: './assets/geneva.jpg',
+            style: 'mapbox://styles/tugaworldtravel/cm5idrk8q001w01rzd6ts6zy1',
+            description: 'Geneva, Switzerland, is a picturesque city nestled along the shores of Lake Geneva, surrounded by the Alps and Jura mountains.',
             location: {
-                center: [20, 40],
-                zoom: 2.3,
-                pitch: -10,
+                center: [6.15116, 46.20595],
+                zoom: 12.52,
+                pitch: 8.01,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'Argentina-chapter',
+            alignment: 'fully',
+            hidden: false,
+            title: 'Buenos Aires',
+            image: './assets/buenos-aires.jpg',
+            description: 'Buenos Aires, the capital of Argentina, is a dynamic city known for its European-inspired architecture, vibrant tango culture, and rich culinary scene.',
+            location: {
+                center: [-58.54195, -34.71600],
+                zoom: 4,
+                pitch: 0,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [
-                () => {
-                    document.body.style.backgroundImage = "url('./assets/SOS_14.JPG')";
-                    document.body.style.backgroundSize = 'cover';
-                    document.body.style.backgroundPosition = 'center';
-                    document.body.style.backgroundAttachment = 'fixed';
-                    document.getElementById('map').style.display = 'none';
-                }
-            ],
-            onChapterExit: [
-                () => {
-                    document.body.style.backgroundImage = 'none';
-                    document.getElementById('map').style.display = 'block';
-                }
-            ]
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'Tokyo-chapter',
+            alignment: 'center',
+            hidden: false,
+            title: 'Tokyo',
+            image: './assets/tokyo.jpg',
+            description: 'Tokyo, Japan\'s bustling capital, is known for its skyscrapers, shopping, and pop culture.',
+            location: {
+                center: [139.6917, 35.6895],
+                zoom: 10,
+                pitch: 45,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
         }
     ]
 };
